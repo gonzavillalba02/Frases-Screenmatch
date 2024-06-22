565471d832fb86166f4a805ee8eea8dba999a238
Gonza <gonzavillalba123@gmail.com>
2024-06-22 19:10:07 -0300

    Proyecto

---
+++ screenmatch-frases/src/main/java/com/aluracursos/screenmatch_frases/Repository/FraseRepository.java
@@ -0,0 +1,11 @@
+package com.aluracursos.screenmatch_frases.Repository;
+
+import com.aluracursos.screenmatch_frases.model.Frase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface FraseRepository  extends JpaRepository<Frase, Long> {
+
+    @Query("SELECT f FROM Frase f ORDER BY FUNCTION('RANDOM') LIMIT 1")
+    public Frase obtenerFraseAleatoria();
+}
